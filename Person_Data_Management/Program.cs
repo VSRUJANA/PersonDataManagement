e571b9de879e871b7b6f1724a957635e95a1644a
VSRUJANA <srujana.valavala@gmail.com>
2020-10-20 00:15:22 +0530

    [srujana] Add. Method to check whether there are records with age between 13 to 18 added

--- Person_Data_Management/Program.cs
+++ Person_Data_Management/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Person_Data_Management
 {
@@ -10,6 +11,12 @@ namespace Person_Data_Management
             Console.WriteLine("Person Data Management System");
             List<Person> listPersonInCity = new List<Person>();
             AddRecords(listPersonInCity);
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Retrieving top 2 records from the list with age is less than 60");
+            Retrieving_TopTwoRecord_ForAge_LessThanSixty(listPersonInCity);
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Checking whether there are teenagers in the list");
+            CheckingForTeenagerPerson(listPersonInCity);
         }
         private static void AddRecords(List<Person> listPersonInCity)
         {
@@ -22,6 +29,24 @@ namespace Person_Data_Management
             listPersonInCity.Add(new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65));
             listPersonInCity.Add(new Person("203456883", "Mac", "126 Province Ave, Baltimore,NY", 85));
             listPersonInCity.Add(new Person("203456884", "SAM", "126 Province Ave, Baltimore,NY", 95));
+        }
+        private static void Retrieving_TopTwoRecord_ForAge_LessThanSixty(List<Person> listPersonsInCity)
+        {
+            foreach (Person person in listPersonsInCity.FindAll(e => (e.Age < 60)).Take(2).ToList())
+            {
+                Console.WriteLine("Name :" + person.Name + "\tAge :" + person.Age);
+            }
+        }
+        private static void CheckingForTeenagerPerson(List<Person> listPersonsInCity)
+        {
+            if (listPersonsInCity.Any(e => e.Age >= 13 && e.Age < 19))
+            {
+                Console.WriteLine("Yes, there are some teenagers in the list");
+            }
+            else
+            {
+                Console.WriteLine("There are no teenagers in the list");
+            }
         }
     }
 }
